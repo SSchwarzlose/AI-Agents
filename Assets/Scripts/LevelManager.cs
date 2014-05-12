c2657c6bfbc09a8193a749688589e02b27219d13
SSchwarzlose <saschaschwarzlose@web.de>
2014-05-12 16:20:27 +0200

    dont know what

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LevelManager : MonoBehaviour {
-
+public class LevelManager : MonoBehaviour
+{
+    
+    private EventManager _event = new EventManager();
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+	    EventManager.onPlayerEnter += StartNewMission;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    void StartNewMission()
+    {
+        Debug.Log("New Mission started!");
+    }
+
+    // Update is called once per frame
+	void Update () 
+    {
 	
 	}
 }
